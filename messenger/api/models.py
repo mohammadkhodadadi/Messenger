7afa19054a20dc4490f3faf91ca588741b8cdd92
Mohammad Khodadadi <moh@khd.com>
2023-09-09 04:43:53 -0700

    create many to many relationship between Chat and User models through Membership middle model

--- messenger/api/models.py
+++ messenger/api/models.py
@@ -19,11 +19,19 @@ class Message(models.Model):
         max_length=1, choices=MESSAGE_TYPE, default=TEXT)
     insert_time = models.DateTimeField(auto_now_add=True)
     update_time = models.DateTimeField(auto_now=True)
+    owner = models.ForeignKey(User, on_delete=models.PROTECT)
 
 
 class Chat(models.Model):
     message = models.ManyToManyField(Message)
     type = models.SmallIntegerField(default=1)  # MESSAGE_TYPE = 1 means regular chat
+    member = models.ManyToManyField(User, through='Membership')
+
+
+class Membership(models.Model):
+    person = models.ForeignKey(User, on_delete=models.CASCADE)
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    date_joined = models.DateField()
 
 
 class Contact(models.Model):
